9d88571bb213b3f0d1ffd0bff7f35faf3191e68c
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-10-14 14:30:33 -0300

    ci: Fix error repository name

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION'){
-      config.base = '/lab-ds-Teste'
+      config.base = '/lab-ds-Teste/'
     }
 
     return config
